fae79b57c7fb1d303e03591433c7f834551cc279
Yunsung Chung <cys1102@gmail.com>
2021-09-30 23:40:50 +1000

    Commit utils

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,19 +2,76 @@ import csv
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("open/train_data.csv", engine="python", error_bad_lines=False)
-nan_list = [27556, 34092, 34201, 66897, 133273, 216203]
-data = data[~data["id"].isin(nan_list)].reset_index(drop=True)
 
-train_data, val_data = train_test_split(data, test_size=0.1, random_state=42)
+def len_summary(df):
+    return len(df["summary"].split())
 
-with open("open/train_gboot.tsv", "w", encoding="utf-8", newline="") as f:
+
+def len_text(df):
+    return len(df["text"].split())
+
+
+def count_apply(df):
+    cnt = 0
+    for word in df["summary"].split():
+        if word in df["text"]:
+            cnt += 1
+    return cnt
+
+
+def count_all_apply(df):
+    return len(df["summary"].split())
+
+
+df_train = pd.read_csv("open/train_data.csv", engine="python", error_bad_lines=False)
+
+# add new columns
+nan_list = [0] * len(df_train)
+df_train["len_text_split"] = nan_list
+df_train["len_summary_split"] = nan_list
+df_train["is_it_summary"] = nan_list
+
+# count words of summary and input text
+df_train.loc[~df_train["summary"].isna(), ["len_text_split"]] = df_train[
+    ~df_train["summary"].isna()
+].apply(len_text, axis=1)
+df_train.loc[~df_train["summary"].isna(), ["len_summary_split"]] = df_train[
+    ~df_train["summary"].isna()
+].apply(len_summary, axis=1)
+
+# compare lenth of words between summary and input text
+df_train["is_it_summary"] = df_train["len_summary_split"] / df_train["len_text_split"]
+
+# add new columns
+df_train["cnt"] = nan_list
+df_train["cnt_all"] = nan_list
+df_train["is_it_extractive"] = nan_list
+
+# calculate the relativeness between summary and input text
+df_train.loc[~df_train["summary"].isna(), ["cnt"]] = df_train[~df_train["summary"].isna()].apply(
+    count_apply, axis=1
+)
+df_train.loc[~df_train["summary"].isna(), ["cnt_all"]] = df_train[
+    ~df_train["summary"].isna()
+].apply(count_all_apply, axis=1)
+df_train["is_it_extractive"] = df_train["cnt"] / df_train["cnt_all"]
+
+# drop improper data
+df_train.dropna(inplace=True)
+df_train.drop(df_train.loc[df_train["is_it_extractive"] < 0.5].index, inplace=True)
+df_train.drop(df_train.loc[df_train["is_it_summary"] > 0.75].index, inplace=True)
+
+
+train_data, val_data = train_test_split(df_train, test_size=0.1, random_state=42)
+
+# save as train and val data
+with open("train_gboot.tsv", "w", encoding="utf-8", newline="") as f:
     writer = csv.writer(f, delimiter="\t", lineterminator="\n")
     writer.writerow(["text", "summary"])
     for idx, row in train_data.iterrows():
         writer.writerow([row["text"], row["summary"]])
 
-with open("open/val_gboot.tsv", "w", encoding="utf-8", newline="") as f:
+with open("val_gboot.tsv", "w", encoding="utf-8", newline="") as f:
     writer = csv.writer(f, delimiter="\t", lineterminator="\n")
     writer.writerow(["text", "summary"])
     for idx, row in val_data.iterrows():
--- inference.py
+++ inference.py
@@ -1,33 +1,80 @@
+import os
+import numpy as np
 import pandas as pd
 import torch
-from kobart import get_kobart_tokenizer
-from transformers import PreTrainedTokenizerFast
-from transformers.models.bart import BartForConditionalGeneration
+from transformers import BartForConditionalGeneration, PreTrainedTokenizerFast
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 
-test_data = pd.read_csv("open/test.csv", engine="python", error_bad_lines=False)
-sample_submission = pd.read_csv(
-    "open/sample_submission.csv", engine="python", error_bad_lines=False
-)
-test_text = test_data["text"]
-summary = []
-
-model = BartForConditionalGeneration.from_pretrained("kobart_summary")
-# tokenizer = get_kobart_tokenizer()
-tokenizer = PreTrainedTokenizerFast.from_pretrained("gogamza/kobart-summarization")
+class Args:
+    batch_size = 64
+    max_len = 512
 
 
-def text2summary(text):
-    input_ids = tokenizer.encode(text)
-    input_ids = torch.tensor(input_ids)
-    input_ids = input_ids.unsqueeze(0)
-    output = model.generate(input_ids, eos_token_id=1, max_length=512, num_beams=5)
-    output = tokenizer.decode(output[0], skip_special_tokens=True)
-    return output
+class TestDataset(Dataset):
+    def __init__(self, tok, data, max_len, pad_index=0):
+        super().__init__()
+        self.tok = tok
+        self.data = data
+        self.max_len = max_len
+        self.len = len(self.data)
+        self.pad_index = pad_index
+
+    def add_padding_data(self, inputs):
+        if len(inputs) < self.max_len:
+            pad = np.array([self.pad_index] * (self.max_len - len(inputs)))
+            inputs = np.concatenate([inputs, pad])
+        else:
+            inputs = inputs[: self.max_len]
+
+        return inputs
+
+    def __getitem__(self, idx):
+        input_ids = self.tok.encode(self.data[idx])
+        input_ids = self.add_padding_data(input_ids)
+        return np.array(input_ids, dtype=np.int_)
+
+    def __len__(self):
+        return self.len
 
 
-for i in range(len(test_text)):
-    summary.append(text2summary(test_text[i]))
+def text2input(text, tokenizer):
+    return tokenizer.encode(text, return_tensors="pt").unsqueeze(0)
 
-sample_submission["summary"] = summary
-sample_submission.to_csv("submission.csv", index=False)
+
+def setup():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    test_data = pd.read_csv("open/test_data.csv", engine="python", error_bad_lines=False)
+    sample_submission = pd.read_csv(
+        "open/sample_submission.csv", engine="python", error_bad_lines=False
+    )
+    test_text = test_data["text"]
+
+    model = BartForConditionalGeneration.from_pretrained("gogamza/kobart-summarization").to(device)
+    tokenizer = PreTrainedTokenizerFast.from_pretrained("gogamza/kobart-summarization")
+
+    return device, model, tokenizer, sample_submission, test_text
+
+
+if __name__ == "__main__":
+    args = Args()
+    device, model, tokenizer, sample_submission, test_text = setup()
+    summary = []
+
+    test_dataset = TestDataset(tokenizer, test_text, args.max_len)
+    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+
+    for (idx, batch) in enumerate(tqdm(test_dataloader)):
+        pred = model.generate(batch.to(device), eos_token_id=1, max_length=128, num_beams=5)
+        for i in range(len(pred)):
+            summary.append(
+                tokenizer.decode(
+                    pred[i].detach().cpu().squeeze().numpy(),
+                    skip_special_tokens=True,
+                )
+            )
+    sample_submission["summary"] = summary
+    sample_submission.to_csv("submission.csv", index=False, encoding="utf-8")
